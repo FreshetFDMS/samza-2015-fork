741d94a5774225bd669500823472f1b4a60fe49d
Milinda Pathirage <milinda.pathirage@gmail.com>
2015-05-27 16:25:18 -0400

    fixed doc comment issue

--- samza-sql-calcite/src/main/java/org/apache/samza/sql/calcite/schema/AvroSchemaUtils.java
+++ samza-sql-calcite/src/main/java/org/apache/samza/sql/calcite/schema/AvroSchemaUtils.java
@@ -33,7 +33,6 @@ import java.util.List;
 /**
  * Defines method which converts Avro {@link org.apache.avro.Schema} to {@link org.apache.calcite.rel.type.RelDataType}
  * and vice versa.
- * <p/>
  * <p>Inspired by parquet-mr.</p>
  */
 public class AvroSchemaUtils {
